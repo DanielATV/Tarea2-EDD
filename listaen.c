04359def26e573a6d761310e7a152f30dd7aa356
godinezseba <sebastian.godinez@hotmail.com>
2017-05-17 17:09:15 -0400

    Update listaen.c

--- listaen.c
+++ listaen.c
@@ -1,1 +1,16 @@
 #include "lista_en.h"
+
+
+int insert(elemento item){
+	tNodo *aux = curr->sig;
+	curr->sig = (tNodo*)malloc(sizeof(tNodo));
+	if (curr->sig == NULL){
+		curr->sig = aux;
+		return 0; //Retorna 0 si falla.
+	}
+	curr->sig->info = item;
+	curr->sig->sig = aux;
+	if(curr == tail) tail = curr->sig;
+	listSize++;
+	return 1; //La operacion ocurrio con exito
+}
