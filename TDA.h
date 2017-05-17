7b85d0217b5510eff2d9a9fc954be63a8e90b2d8
godinezseba <sebastian.godinez@hotmail.com>
2017-05-17 16:04:23 -0400

    Update TDA.h

--- TDA.h
+++ TDA.h
@@ -9,44 +9,41 @@ struct nodo *sig; // puntero autoreferencial
 // al proximo nodo
 } tNodo;
 
-tLista {
-tNodo *head;
-tNodo *tail;
-tNodo *curr;
-unsigned int listSize;
-unsigned int pos; // posición actual en la lista
-public:
- // métodos de la clase
-};
+//Estructura de la lista:
+typedef struct Lista{
+ tNodo *head; //Primer elemento
+ tNodo *tail; //Ultimo elemento
+ tNodo *curr; //Elemento actual
+ unsigned int listSize; //Tamaño de la lista
+ unsigned int pos; // Posición actual en la lista
+} tLista;
 
 // borra todos los elementos de la lista, reinicializándola vacía D
-void clear ()
+void clear ();
 // inserta un elemento en la posición actual de la lista S
-int insert (tElemLista item)
+int insert (tElemLista item);
 // agrega un elemento al final de la lista ******
-int append (tElemLista item)
+int append (tElemLista item);
 // borra el elemento actual y retorna su valor  F
-tElemLista remove ()
-
- // mueve la posición actual al comienzo de la lista D
-void moveToStart ()
+tElemLista remove ();
+// mueve la posición actual al comienzo de la lista D
+void moveToStart ();
 // mueve la posición actual al final de la lista S 
-void moveToEnd ()
+void moveToEnd ();
 // mueve la posición actual al siguiente elemento de la lista.
 // No produce cambios si está en la cabeza de la lista F
-void next ()
+void next ();
 // mueve la posición actual al elemento anterior de la lista.
 // No produce cambios si está en la cola de la lista D
-void prev ()
-
+void prev ();
 // retorna el número de elementos en la lista s
-int length ()
+int length ();
 // retorna la posición del elemento actual F
-int currPos ()
+int currPos ();
 // mueve la posición actual a una especificada D
-void moveToPos (int pos)
+void moveToPos (int pos);
 // obtiene el valor del elemento actual de la lista S
-tElemLista getValue ()
+tElemLista getValue ();
  
  
 
