c203dc0ef743c9de26b4eac2986b46f72640ed5e
DanielATV <daniel.torov@sansano.usm.cl>
2017-05-17 14:06:18 -0400

    Update TDA.h

--- TDA.h
+++ TDA.h
@@ -1,8 +1,7 @@
-typedef struct nodo {
-tElemLista info; // el elemento en sí
-struct nodo *sig; // puntero autoreferencial
-// al proximo nodo
-} tNodo;
+typedef struct node {
+    int val;
+    struct node * next;
+} node_t;
 
 tLista {
 tNodo *head;
@@ -18,9 +17,9 @@ public:
 void clear ()
 // inserta un elemento en la posición actual de la lista
 int insert (tElemLista item)
-// agrega un elemento al final de la lista
+// agrega un elemento al final de la lista ******
 int append (tElemLista item)
-// borra el elemento actual y retorna su valor
+// borra el elemento actual y retorna su valor 
 tElemLista remove ()
 
  // mueve la posición actual al comienzo de la lista
@@ -34,7 +33,7 @@ void next ()
 // No produce cambios si está en la cola de la lista
 void prev ()
 
-// retorna el número de elementos en la lista
+// retorna el número de elementos en la lista ********
 int length ()
 // retorna la posición del elemento actual
 int currPos ()
