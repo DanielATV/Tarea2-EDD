ac087d2c5c267c99ae2f801f18264b0c98bf11e7
DanielATV <daniel.torov@sansano.usm.cl>
2017-05-17 14:41:11 -0400

    Update TDA.h

--- TDA.h
+++ TDA.h
@@ -14,33 +14,33 @@ public:
  // métodos de la clase
 };
 
-// borra todos los elementos de la lista, reinicializándola vacía
+// borra todos los elementos de la lista, reinicializándola vacía D
 void clear ()
-// inserta un elemento en la posición actual de la lista
+// inserta un elemento en la posición actual de la lista S
 int insert (tElemLista item)
 // agrega un elemento al final de la lista ******
 int append (tElemLista item)
-// borra el elemento actual y retorna su valor 
+// borra el elemento actual y retorna su valor  F
 tElemLista remove ()
 
- // mueve la posición actual al comienzo de la lista
+ // mueve la posición actual al comienzo de la lista D
 void moveToStart ()
-// mueve la posición actual al final de la lista
+// mueve la posición actual al final de la lista S 
 void moveToEnd ()
 // mueve la posición actual al siguiente elemento de la lista.
-// No produce cambios si está en la cabeza de la lista
+// No produce cambios si está en la cabeza de la lista F
 void next ()
 // mueve la posición actual al elemento anterior de la lista.
-// No produce cambios si está en la cola de la lista
+// No produce cambios si está en la cola de la lista D
 void prev ()
 
-// retorna el número de elementos en la lista ********
+// retorna el número de elementos en la lista ******** 
 int length ()
-// retorna la posición del elemento actual
+// retorna la posición del elemento actual S
 int currPos ()
-// mueve la posición actual a una especificada
+// mueve la posición actual a una especificada F 
 void moveToPos (int pos)
-// obtiene el valor del elemento actual de la lista
+// obtiene el valor del elemento actual de la lista D
 tElemLista getValue ()
  
  
