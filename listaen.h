622eb55ceeee4a869dac3252f6829cdd89684e46
godinezseba <sebastian.godinez@hotmail.com>
2017-05-17 19:11:11 -0400

    Update listaen.h

--- listaen.h
+++ listaen.h
@@ -21,35 +21,35 @@ typedef struct Lista{
  tNodo *curr; //Elemento actual
  unsigned int listSize; //Tamaño de la lista
  unsigned int pos; // Posición actual en la lista
- 
+} tLista;
+
  // crea la lista LISTO
  tLista *crearLista();
  // borra todos los elementos de la lista, reinicializándola vacía D
- void clear ();
+ void clear (lLista*);
  // inserta un elemento en la posición actual de la lista S LISTO
- int insert (elemento);
+ int insert (lLista*,elemento);
  // agrega un elemento al final de la lista LISTO
- int append (tElemLista item);
- // borra el elemento actual y retorna su valor  F
- tElemLista remove ();
+ int append (lLista*,tElemLista);
+ // borra el elemento actual y retorna su valor  F LISTO
+ elemento remove (lLista*);
  // mueve la posición actual al comienzo de la lista D
- void moveToStart ();
+ void moveToStart (lLista*);
  // mueve la posición actual al final de la lista S LISTO
- void moveToEnd ();
+ void moveToEnd (lLista*);
  // mueve la posición actual al siguiente elemento de la lista.
  // No produce cambios si está en la cabeza de la lista F LISTO
- void next ();
+ void next (lLista*);
  // mueve la posición actual al elemento anterior de la lista.
  // No produce cambios si está en la cola de la lista D
- void prev ();
+ void prev (lLista*);
  // retorna el número de elementos en la lista s LISTO
- int length ();
+ int length (lLista*);
  // retorna la posición del elemento actual F LISTO
- int currPos ();
+ int currPos (lLista*);
  // mueve la posición actual a una especificada D
- void moveToPos (int pos);
+ void moveToPos (lLista*,int);
  // obtiene el valor del elemento actual de la lista S LISTO
- tElemLista getValue ();
-} tLista;
+ tElemLista getValue (lLista*);
 
 #endif
