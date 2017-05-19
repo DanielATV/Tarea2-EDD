e6d9b0f1997c46e17b5d80e146609f5435303c80
DanielATV <daniel.torov@sansano.usm.cl>
2017-05-19 16:51:13 -0400

    Update main.c

--- main.c
+++ main.c
@@ -2,8 +2,32 @@
 #include "lista_en.h"
 
 void free(tLista *lista1,tLista *lista2, int byte){}
-int malloc(tLista *lista1,tLista *lista2, int byte){}
-void check(tLista *lista){}
+int malloc(tLista *lista1,tLista *lista2, int byte){
+  int i,j,k,n,m;
+  elemento a,b;
+  j = lenght(lista1);
+  for(moveToStart(lista1);i = currentPos(lista1)<j; next(lista1)){
+    a = getValue(lista1);
+    n = a.comienzo;
+    m = a.final;
+    k = m - n + 1;
+    if (byte == k){
+      append(lista2,a);
+      remove(lista1);
+      return 0;
+    }
+    else if ( byte < k){
+      b.final = a.final;
+      a.final = a.comienzo + k - 1;
+      b.inicial = a.comienzo + k;
+      append(lista2, a);
+      remove(lista1);
+      append(llista1, b);
+    }
+  }
+  return -1;
+}
+void check(tLista *lista1){}
 int main(){
   return 0;
 }
