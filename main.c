ca28ea09efac1ea460bd1ec22e40267ed725ab96
DanielATV <daniel.torov@sansano.usm.cl>
2017-05-19 15:08:13 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
+#include <stdio.h>
 #include "lista_en.h"
 
+void free(tLista *lista1,tLista *lista2, int byte){}
+int malloc(tLista *lista1,tLista *lista2, int byte){}
+void check(tLista *lista){}
 int main(){
   return 0;
 }
