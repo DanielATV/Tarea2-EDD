08689226517c37372056ba9821ab6fc6df07fa78
godinezseba <sebastian.godinez@hotmail.com>
2017-05-19 20:31:20 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
-#include "lista_en.h"
+elemento lremove(tLista *l){
+	tNodo *aux = l->curr->sig;
+	elemento item = l->curr->sig->info;
 
-int main(){
-  return 0;
+	free((void *)aux);
+	if (l->curr->sig->sig == NULL){ 
+		l->tail = l->curr;
+	}
+	else{	
+		l->curr->sig = l->curr->sig->sig;
+	}
+	free((void *)aux);
+	l->pos --;
+	l->listSize--;
+	return item;	
 }
