1ecf613027521c9616182427eb8315a9d056d7af
godinezseba <sebastian.godinez@hotmail.com>
2017-05-19 20:03:50 -0400

    Update funciones.c

--- funciones.c
+++ funciones.c
@@ -35,3 +35,31 @@ void juntartodas(tLista *l)
 	}
 
 }
+
+int Malloc(tLista *lista1,tLista *lista2, int byte){
+  int k,n,m;
+  elemento a,b;
+  for(moveToStart(lista1);currPos(lista1) < length(lista1); next(lista1)){
+    a = getValue(lista1);
+    n = a.comienzo;
+    m = a.final;
+    k = m - n + 1;
+    if (byte == k){
+      append(lista2,a);
+      lremove(lista1);
+      return 0;
+    }
+    else if ( byte < k){
+      b.final = a.final;
+      a.final = a.comienzo + byte - 1;
+      b.comienzo = a.comienzo + byte;
+      append(lista2, a);
+      insert(lista1, b);
+      next(lista1);
+      lremove(lista1);
+
+      return 0;
+    }
+  }
+  return -1;
+}
