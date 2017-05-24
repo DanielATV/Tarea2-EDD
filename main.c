08295677765f1e369332de8af23be6c580ea5248
godinezseba <sebastian.godinez@hotmail.com>
2017-05-23 22:51:41 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,41 @@
-elemento lremove(tLista *l){
-	tNodo *aux = l->curr->sig;
-	elemento item = l->curr->sig->info;
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "funciones.c" //depende de como lo tengas tu
 
-	free((void *)aux);
-	if (l->curr->sig->sig == NULL){ 
-		l->tail = l->curr;
-	}
-	else{	
-		l->curr->sig = l->curr->sig->sig;
-	}
-	free((void *)aux);
-	l->pos --;
-	l->listSize--;
-	return item;	
+void creardat(){
+	FILE *fp;
+	fp = fopen("input.dat","w");
+	fprintf(fp, "%d\n",100 );
+	fprintf(fp, "%d\n",9 );
+
+	fprintf(fp, "%s %d\n","malloc",10);
+	fprintf(fp, "%s %d\n","malloc",20);
+	fprintf(fp, "%s %d\n","malloc",10);
+	fprintf(fp, "%s %d\n","malloc",40);
+	fprintf(fp, "%s %d\n","malloc",21);
+	fprintf(fp, "%s %d\n","free",31);
+	fprintf(fp, "%s %d\n","malloc",5);
+	fprintf(fp, "%s %d\n","malloc",10);
+	fprintf(fp, "%s %d\n","malloc",5);
+	fclose(fp);
+
+}
+
+
+
+
+int main(){
+	//creardat();
+	FILE *fp;
+	char operacion[10];
+	int m,n,valor;
+	fp = fopen("input.dat","r");
+	fscanf(fp,"%d\n%d\n",&m,&n); //nose si esta bien
+
+	fscanf(fp,"%s %d\n",&operacion,&valor ); //dentro de un ciclo para n lineas
+
+
+	return 0;
+	
 }
